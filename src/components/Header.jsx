a359cad5ad1f2df95601ee08b170280f920dcd3c
mknyambura <kariuki.mercie@gmail.com>
2023-06-08 22:51:43 +0300

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode } from 'swiper' 
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+import bg1 from '../assets/bg1.png'
+import bg2 from '../assets/bg2.png'
+import yellowBox from '../assets/yellow-box.png'
+
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex flex-row items-center'>
+        <div className='flex flex-col gap-8 px-20'>
+            <h1 className='font-bold text-5xl'>We help grow the tech industry</h1>
+            <p className='opacity-70'>
+                We will help to develop every smallest thing into a big one for your company
+            </p>
+            <div className='flex flex-row gap-8'>
+                <button className='bg-[#FF723A] px-5 py-2 rounded-md'>Contact Us</button>
+                <button className='text-[#FF723A] px-5 py-2 rounded-md border border-[#FF723A]'>Our Works</button>
+            </div>
+        </div>
+        <Swiper
+            freeMode={true}
+            grabCursor={true}
+            modules={FreeMode}
+            slidesPerView={1}
+            spaceBetween={30}
+            className=' text-black gap-8 flex flex-row w-3/4 scrollbar-hide px-28'
+        >
+            <SwiperSlide>
+                <div className='w-3/4'>
+                    <img src={bg1} alt="" width={500}/>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className='flex items-center relative'>
+                    <img src={yellowBox} alt="" />
+                    <div className='absolute top-[-2%] left-[-15%]'>
+                        <img src={bg2} alt="" width={500} />
+                    </div>
+                </div>
+            </SwiperSlide>
+        </Swiper>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-between'>
+    <div className='flex flex-row items-center justify-between px-14 py-12'>
         <h1 className='font-bold  text-2xl'><span className='text-[#FF723A]'>R</span>adiant</h1>
-        <div className='flex flex-row gap-5'>
+        <div className='flex flex-row gap-4'>
             <h1 className='opacity-70'>Home</h1>
             <h1 className='opacity-70'>About Us</h1>
             <h1 className='opacity-70'>Services</h1>
@@ -14,9 +14,9 @@ const Navbar = () => {
             <h1 className='opacity-70'>FAQs</h1>
             <h1 className='opacity-70'>Contact Us</h1>
         </div>
-        <div className='flex flex-row gap-5'>
-            <button className='opacity-70 hover:outline-[#FF723A]'>Login</button>
-            <button className='px-5 py-2 rounded-md bg-[#FF723A] '>Sign Up</button>
+        <div className='flex flex-row gap-4'>
+            <button className='px-4 py-2 rounded-md opacity-70 hover:border hover:border-[#FF723A]'>Login</button>
+            <button className='px-4 py-2 rounded-md bg-[#FF723A] '>Sign Up</button>
         </div>
     </div>
   )
